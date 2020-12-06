0d9c3deebb97fae48e7074356150a7691b8ba403
Keneth3000 <kenethra.74@gmail.com>
2020-12-05 20:46:39 -0600

    [feat]: use the setVisible method for panels

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+    @FXML private AnchorPane locationPanel, userPanel;
+    @FXML private ImageView locationArrow, userArrow;
+
+    public void onExitButtonClicked(MouseEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void onUserButtonClicked(MouseEvent event) {
+        userPanel.setVisible(true);
+        userArrow.setVisible(true);
+
+        locationPanel.setVisible(false);
+        locationArrow.setVisible(false);
+    }
+
+    public void onLocationButtonClicked(MouseEvent event) {
+        locationPanel.setVisible(true);
+        locationArrow.setVisible(true);
+
+        userPanel.setVisible(false);
+        userArrow.setVisible(false);
+    }
 }
